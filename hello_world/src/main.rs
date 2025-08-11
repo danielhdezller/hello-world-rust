59d1519d6d84636f3afc5bd06a039e5db8869794
Daniel <dhdezllerena94@gmail.com>
2025-08-11 12:44:15 +0200

    feat: new fn sum fruits

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    println!("Lets see what happens here:");
+    sumFruits();
+}
+
+fn sumFruits() {
+ let apple = 5;
+ let banana = 10;
+ let total = apple + banana;
+ println!("The total number of fruits is: {total}");
 }
