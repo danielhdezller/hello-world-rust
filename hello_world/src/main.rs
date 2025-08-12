d0a3c0791064e236cf545994a9da5243d9a21a27
Daniel <dhdezllerena94@gmail.com>
2025-08-12 10:55:16 +0200

    feat: compiler directive, constants and scopes

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,12 +1,35 @@
+const STORE_SPACE: i32 = 100;
+type Units = i32;
+
 fn main() {
     println!("Hello, world!");
     println!("Lets see what happens here:");
-    sumFruits();
+    sum_fruits();
+
+    // compiler directive
+    #[allow(unused_variables)]
+    let unused_variable = "this variable its unused";
 }
 
-fn sumFruits() {
- let apple = 5;
- let banana = 10;
- let total = apple + banana;
- println!("The total number of fruits is: {total}");
+fn sum_fruits() {
+    let apple = 5;
+    let banana = 10;
+    let total: Units = apple + banana;
+    //  STORE_SPACE = 10;
+
+    println!("The total space is: {STORE_SPACE}");
+    println!("The total number of fruits is: {total}");
+    println!(
+        "I have {0} apples and {1} bananas, I cant believe I have all of this apples {0}",
+        apple, banana
+    );
+
+    un_used_variables()
+}
+
+// compiler directive
+#[allow(unused_variables)]
+fn un_used_variables() {
+    let unused_variable = "this variable its unused";
+    let unused_variable_two = "this variable its unused";
 }
